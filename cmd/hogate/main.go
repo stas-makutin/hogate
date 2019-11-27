871dc1b6813958ab1f4e2f6cadd701054e4b68e2
stas-makutin <github@makutin.net>
2019-11-27 11:37:24 -0500

    fix file encoding (remove utf8 bom header)

--- cmd/hogate/main.go
+++ cmd/hogate/main.go
@@ -1,20 +1,25 @@
-﻿package main
+package main
 
 import (
+	"log"
+
 	"github.com/kardianos/service"
 )
 
+var logger service.Logger
+
 type program struct{}
 
-func (p *program) run() {
-}
-
 func (p *program) Start(s service.Service) error {
+	// Start should not block. Do the actual work async.
 	go p.run()
 	return nil
 }
-
+func (p *program) run() {
+	// Do work here
+}
 func (p *program) Stop(s service.Service) error {
+	// Stop should not block. Return with a few seconds.
 	return nil
 }
 
@@ -22,16 +27,17 @@ func main() {
 	svcConfig := &service.Config{
 		Name:        "hogate",
 		DisplayName: "Home HTTP Gateway",
-		Description: "Home HTTP Gateway",
+		Description: "Home HTTP Gateway.",
 	}
+
 	prg := &program{}
 	s, err := service.New(prg, svcConfig)
 	if err != nil {
-		//log.Fatal(err)
+		log.Fatal(err)
 	}
-	logger, err := s.Logger(nil)
+	logger, err = s.Logger(nil)
 	if err != nil {
-		//log.Fatal(err)
+		log.Fatal(err)
 	}
 	err = s.Run()
 	if err != nil {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/stas-makutin/hogate
 
 go 1.13
 
-require github.com/kardianos/service v1.0.0 // indirect
+require (
+	github.com/kardianos/service v1.0.0
+	golang.org/x/sys v0.0.0-20190215142949-d0b11bdaac8a // indirect
+)
