295465e78b797d3e9c7af29b4ebe8896446b9621
stas-makutin <github@makutin.net>
2019-11-27 09:43:30 -0500

    first cut

---
+++ cmd/hogate/main.go
@@ -0,0 +1,40 @@
+﻿package main
+
+import (
+	"github.com/kardianos/service"
+)
+
+type program struct{}
+
+func (p *program) run() {
+}
+
+func (p *program) Start(s service.Service) error {
+	go p.run()
+	return nil
+}
+
+func (p *program) Stop(s service.Service) error {
+	return nil
+}
+
+func main() {
+	svcConfig := &service.Config{
+		Name:        "hogate",
+		DisplayName: "Home HTTP Gateway",
+		Description: "Home HTTP Gateway",
+	}
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		//log.Fatal(err)
+	}
+	logger, err := s.Logger(nil)
+	if err != nil {
+		//log.Fatal(err)
+	}
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/stas-makutin/hogate
+
+go 1.13
+
+require github.com/kardianos/service v1.0.0 // indirect
